5575a3c4f92cdce0eeadde0c24f4964340187f5a
safwaf <safwafsafwaf@gmail.com>
2017-06-14 15:23:33 -0600

    bfca

--- P2P chat/Form1.Designer.cs
+++ P2P chat/Form1.Designer.cs
@@ -31,6 +31,10 @@
             this.ConvoBox = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.HostButton = new System.Windows.Forms.Button();
+            this.IPBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.JoinButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // ConvoBox
@@ -57,16 +61,54 @@
             this.textBox1.Size = new System.Drawing.Size(179, 20);
             this.textBox1.TabIndex = 2;
             // 
+            // HostButton
+            // 
+            this.HostButton.Location = new System.Drawing.Point(285, 54);
+            this.HostButton.Name = "HostButton";
+            this.HostButton.Size = new System.Drawing.Size(75, 23);
+            this.HostButton.TabIndex = 3;
+            this.HostButton.Text = "Host";
+            this.HostButton.UseVisualStyleBackColor = true;
+            // 
+            // IPBox
+            // 
+            this.IPBox.Location = new System.Drawing.Point(285, 28);
+            this.IPBox.Name = "IPBox";
+            this.IPBox.Size = new System.Drawing.Size(186, 20);
+            this.IPBox.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(282, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Host IP";
+            // 
+            // JoinButton
+            // 
+            this.JoinButton.Location = new System.Drawing.Point(396, 54);
+            this.JoinButton.Name = "JoinButton";
+            this.JoinButton.Size = new System.Drawing.Size(75, 23);
+            this.JoinButton.TabIndex = 6;
+            this.JoinButton.Text = "Join";
+            this.JoinButton.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(483, 261);
+            this.Controls.Add(this.JoinButton);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.IPBox);
+            this.Controls.Add(this.HostButton);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.ConvoBox);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "P2Pchat Alpha Version";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -77,6 +119,10 @@
         private System.Windows.Forms.ListBox ConvoBox;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button HostButton;
+        private System.Windows.Forms.TextBox IPBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button JoinButton;
     }
 }
 
--- P2P chat/Form1.cs
+++ P2P chat/Form1.cs
@@ -16,5 +16,22 @@ namespace P2P_chat
         {
             InitializeComponent();
         }
+
+        private byte[] ByteString(string s)
+        {
+            char[] c = s.ToCharArray();
+            byte[] b = null;
+            int l = s.Length;
+            for (int i=0;i<l;i++)
+            {
+                b[i] = (byte)c[i];
+            }
+            return b;
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
